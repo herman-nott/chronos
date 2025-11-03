574c1b4505867bbb5fee6c21ebd81d0d3a2d6b22
herman-nott <mcgerman2006@gmail.com>
2025-11-03 23:54:17 +0100

    first version of database is done

--- backend/database/models/EmailVerification.js
+++ backend/database/models/EmailVerification.js
@@ -18,7 +18,7 @@ const EmailVerificationSchema = new mongoose.Schema({
         type: Date, 
         required: true 
     }
-});
+}, { timestamps: true });
 
 const EmailVerification = mongoose.model('EmailVerification', EmailVerificationSchema);
 
